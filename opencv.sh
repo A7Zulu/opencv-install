8afcd90626c354f5d0a3e10a85474739b80afe53
A7Zulu <A7Zulu@gmail.com>
2014-01-03 18:56:47 +0000

    added a few lines

--- opencv.sh
+++ opencv.sh
@@ -1,11 +1,16 @@
 cd /usr/local
-git clone https://github.com/Itseez/opencv.git
-cd opencv
 sudo apt-get -y install build-essential cmake pkg-config
 sudo apt-get -y install libjpeg62-dev 
 sudo apt-get -y install libtiff4-dev libjasper-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install python-dev python-numpy
+sudo apt-get -y install libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get -y install libtbb-dev
+sudo apt-get -y install libqt4-dev
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+mkdir build
+cd build
 sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_opencv_python=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 sudo make -j2
 sudo make install
